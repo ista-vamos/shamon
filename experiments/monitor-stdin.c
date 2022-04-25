4f32628d0490c556547d90e91b895c559c4258d9
Marek Chalupa <mchqwerty@gmail.com>
2022-04-25 17:06:59 +0200

    shamon: use buffer_events instead of get_next_event
    We need the buffering be as efficient as possible and this
    way we can buffer more events at once.

--- experiments/monitor-stdin.c
+++ experiments/monitor-stdin.c
@@ -12,16 +12,20 @@ int main(void) {
     shm_stream *stdin_stream = shm_create_stdin_stream();
     shamon_add_stream(shmn, stdin_stream);
 
-    while ((ev = shamon_get_next_ev(shmn))) {
-        printf("Event id %lu\n", shm_event_id(ev));
-        shm_kind kind = shm_event_kind(ev);
-        printf("Event kind %lu ('%s')\n", kind, shm_event_kind_name(kind));
-        printf("Event size %lu\n", shm_event_size(ev));
-        shm_event_stdin *stdin_ev = (shm_event_stdin *) ev;
-        printf("Event time %lu\n", stdin_ev->time);
-        printf("Data: fd: %d, size: %lu:'%s'\n",
-               stdin_ev->fd, stdin_ev->str_ref.size,
-	       stdin_ev->str_ref.data);
+    puts("Created STDIN stream");
+
+    while (1) {
+        while ((ev = shamon_get_next_ev(shmn))) {
+            printf("Event id %lu\n", shm_event_id(ev));
+            shm_kind kind = shm_event_kind(ev);
+            printf("Event kind %lu ('%s')\n", kind, shm_event_kind_name(kind));
+            printf("Event size %lu\n", shm_event_size(ev));
+            shm_event_stdin *stdin_ev = (shm_event_stdin *) ev;
+            printf("Event time %lu\n", stdin_ev->time);
+            printf("Data: fd: %d, size: %lu:'%s'\n",
+                   stdin_ev->fd, stdin_ev->str_ref.size,
+               stdin_ev->str_ref.data);
+        }
     }
     // TODO: make this a callback of shamon_destroy()
     shm_destroy_stdin_stream((shm_stream_stdin *)stdin_stream);
