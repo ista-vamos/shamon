24be1c34ba4dea1d3f8e28cf51b163034262d60f
Fabian Muehlboeck <fabianmuehlboeck@users.noreply.github.com>
2022-06-16 22:30:50 +0200

    bank experiments

--- experiments/interact.c
+++ experiments/interact.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,61 +6,90 @@
 
 char buffer[BUFSIZE];
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	if(argc<2)
+	if (argc < 2)
 	{
-		printf("Usage: interact [filename]\n");
+		printf("Usage: interact [filename] {[}reportfile}\n");
 		return 0;
 	}
-	FILE * inputfile = fopen(argv[1], "r");
-	if(inputfile==NULL)
+	FILE *inputfile = fopen(argv[1], "r");
+	if (inputfile == NULL)
 	{
 		printf("Could not find input file\n");
 		return 0;
 	}
-	char * line=NULL;
-	size_t linesize=0;
-	char * inputline=NULL;
-	size_t inputlinesize=0;
-	while(getline(&line, &linesize,inputfile)>=0)
+	char *line = NULL;
+	size_t linesize = 0;
+	char *inputline = NULL;
+	size_t inputlinesize = 0;
+	while (getline(&line, &linesize, inputfile) >= 0)
 	{
-		if(linesize==0 && feof(inputfile))
+		if (linesize == 0 && feof(inputfile))
 		{
 			break;
 		}
-		if(getdelim(&inputline, &inputlinesize, '>', stdin)<0)
+		if (getdelim(&inputline, &inputlinesize, '>', stdin) < 0)
 		{
 			return -1;
 		}
-		printf("%s",line);
+		printf("%s", line);
 		fflush(stdout);
 	}
-	if(freopen("/dev/tty","w",stdout)==NULL)
+	FILE *rfile;
+	if (freopen("/dev/tty", "w", stdout) == NULL)
 	{
 		return -1;
 	}
-	while(getdelim(&inputline, &inputlinesize, ':', stdin)>=0)
+	if (argc > 2)
 	{
-		if(strncmp(inputline+strlen(inputline)-17,"Errors generated:",17)==0)
+		rfile = fopen(argv[2], "w");
+	}
+	else
+	{
+		rfile = stdout;
+	}
+	if (!rfile)
+	{
+		return -1;
+	}
+	while (getdelim(&inputline, &inputlinesize, ':', stdin) >= 0)
+	{
+		if (strncmp(inputline + strlen(inputline) - 17, "Errors generated:", 17) == 0)
 		{
-			if(getdelim(&inputline, &inputlinesize, '\n', stdin)<0)
+			if (getdelim(&inputline, &inputlinesize, '\n', stdin) < 0)
 			{
+				if (rfile != stdout)
+				{
+					fclose(rfile);
+				}
 				return -1;
 			}
-			long errorcount=atol(inputline+1);
-			printf("Errors generated: %li\n",errorcount);
+			long errorcount = atol(inputline + 1);
+			fprintf(rfile, "Errors generated: %li\n", errorcount);
 		}
-		else if(strncmp(inputline+strlen(inputline)-5,"Time:",5)==0)
+		else if (strncmp(inputline + strlen(inputline) - 5, "Time:", 5) == 0)
 		{
-			if(getdelim(&inputline, &inputlinesize, 's', stdin)<0)
+			if (getdelim(&inputline, &inputlinesize, 's', stdin) < 0)
 			{
+				if (rfile != stdout)
+				{
+					fclose(rfile);
+				}
 				return -1;
 			}
-			double time = atof(inputline+1);
-			printf("Time: %f\n",time);
+			double time = atof(inputline + 1);
+			fprintf(rfile, "Time: %f\n", time);
+			if (rfile != stdout)
+			{
+				fclose(rfile);
+			}
 			return 0;
 		}
+	}
+	if (rfile != stdout)
+	{
+		fclose(rfile);
 	}
 	return -1;
 	// if(getdelim(&inputline, &inputlinesize, 'T', stdin)<0)
@@ -77,4 +107,3 @@ int main(int argc, char** argv)
 	// }
 	// return 0;
 }
-
