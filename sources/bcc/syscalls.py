f8738f080af42f9351dd50d5c4be98fe0a1edd2a
Marek Chalupa <mchqwerty@gmail.com>
2022-07-11 19:23:22 +0200

    sources/bcc: update syscalls source

--- sources/bcc/syscalls.py
+++ sources/bcc/syscalls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from sys import stderr
-import time
+from time import sleep
 
 from bcc import BPF
 
@@ -150,7 +150,69 @@ TRACEPOINT_PROBE(syscalls, sys_enter_write) {
     if (off >= args->count)
         return 0;
 
-    bpf_trace_printk("EVENT TOO LONG\\n");
+    /* 6th PART */
+    event = buffer.ringbuf_reserve(sizeof(struct event));
+    if (!event) {
+        bpf_trace_printk("FAILED RESERVING SLOT (2) IN BUFFER\\n");
+        return 1;
+    }
+    max_read_len = (args->count - off > BUF_SIZE) ? BUF_SIZE : args->count - off;
+    len = bpf_probe_read_user_str(event->buf, max_read_len, user_buf + off);
+    if (len < 0) {
+        bpf_trace_printk("FAILED READING USER STRING\\n");
+        buffer.ringbuf_discard(event, 0);
+        return 1;
+    } else {
+        bpf_trace_printk("SUBMIT EVENT\\n");
+        event->count = args->count - off;
+        event->fd = args->fd;
+        event->len = len;
+        event->off = off;
+        buffer.ringbuf_submit(event, 0);
+    }
+    off += len;
+    if (off >= args->count)
+        return 0;
+
+    /* 7th PART */
+    event = buffer.ringbuf_reserve(sizeof(struct event));
+    if (!event) {
+        bpf_trace_printk("FAILED RESERVING SLOT (2) IN BUFFER\\n");
+        return 1;
+    }
+    max_read_len = (args->count - off > BUF_SIZE) ? BUF_SIZE : args->count - off;
+    len = bpf_probe_read_user_str(event->buf, max_read_len, user_buf + off);
+    if (len < 0) {
+        bpf_trace_printk("FAILED READING USER STRING\\n");
+        buffer.ringbuf_discard(event, 0);
+        return 1;
+    } else {
+        bpf_trace_printk("SUBMIT EVENT\\n");
+        event->count = args->count - off;
+        event->fd = args->fd;
+        event->len = len;
+        event->off = off;
+        buffer.ringbuf_submit(event, 0);
+    }
+    off += len;
+    if (off >= args->count)
+        return 0;
+
+    /* 7th PART */
+    event = buffer.ringbuf_reserve(sizeof(struct event));
+    if (!event) {
+        bpf_trace_printk("FAILED RESERVING SLOT (2) IN BUFFER\\n");
+        return 1;
+    }
+
+    /* Tell monitor that the text was too long */
+    event->buf[0] = 0;
+    event->count = args->count - off;
+    event->fd = args->fd;
+    event->len = -1;
+    event->off = off;
+    buffer.ringbuf_submit(event, 0);
+
     return 0;
 }
 """
@@ -158,18 +220,38 @@ TRACEPOINT_PROBE(syscalls, sys_enter_write) {
 
 b = BPF(text=src)
 get_data = b['buffer'].event
+partial = ""
+
+def printstderr(x): print(x, file=stderr)
 
 def callback(ctx, data, size):
+    global partial
     event = get_data(data)
     s = event.buf.decode('utf-8', 'ignore')
-    print(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m:",
-          file=stderr)
-    print(f"'{s}'", file=stderr)
+    if event.len == event.count:
+        printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m:")
+        if partial:
+            printstderr(f"{partial}{s}")
+            partial = ""
+        else:
+            printstderr(f"{s}")
+    elif event.len < event.count:
+        printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m")
+        if event.len == -1:
+            printstderr(f"{partial}{s}")
+            printstderr(f"... TEXT TOO LONG, DROPPED {event.count} CHARS")
+            partial = ""
+        else:
+            partial += s
+    else:
+        printstderr('UNHANDLED SITUATION', file=stderr)
+        printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m")
 
 b['buffer'].open_ring_buffer(callback)
 
 try:
     while 1:
         b.ring_buffer_consume()
+        sleep(0.01)
 except KeyboardInterrupt:
     exit()
