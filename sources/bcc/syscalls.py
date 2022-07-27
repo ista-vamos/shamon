f63d92a1af9bef601668e2c19770dd09413ccd15
Marek Chalupa <mchqwerty@gmail.com>
2022-07-27 18:36:05 +0200

    sources/bcc: fix syscalls python source

--- sources/bcc/syscalls.py
+++ sources/bcc/syscalls.py
@@ -14,13 +14,13 @@ submit_next_event=\
 r"""
     event = buffer.ringbuf_reserve(sizeof(struct event));
     if (!event) {
-        bpf_trace_printk("FAILED RESERVING SLOT (2) IN BUFFER\\n");
+        bpf_trace_printk("FAILED RESERVING SLOT IN BUFFER");
         return 1;
     }
-    max_read_len = (args->count - off > BUF_SIZE) ? BUF_SIZE : args->count - off;
-    len = bpf_probe_read_user_str(event->buf, max_read_len, user_buf + off);
-    if (len < 0) {
-        bpf_trace_printk("FAILED READING USER STRING\\n");
+    len = (args->count - off > BUF_SIZE) ? BUF_SIZE : args->count - off;
+    ret = bpf_probe_read_user(event->buf, len, user_buf + off);
+    if (ret != 0) {
+        bpf_trace_printk("FAILED READING USER STRING");
         buffer.ringbuf_discard(event, 0);
         return 1;
     } else {
@@ -38,7 +38,7 @@ r"""
 """
 
 src = r"""
-BPF_RINGBUF_OUTPUT(buffer, 1 << 4);
+BPF_RINGBUF_OUTPUT(buffer, 1 << 6);
 
 #define BUF_SIZE 255
 struct event {
@@ -50,8 +50,8 @@ struct event {
 };
 
 TRACEPOINT_PROBE(syscalls, sys_enter_write) {
-    int len;
-    unsigned int max_read_len, off = 0;
+    int ret;
+    unsigned int len, off = 0;
 
     if (args->fd != 1) {
         return 0; // not interested
@@ -59,36 +59,14 @@ TRACEPOINT_PROBE(syscalls, sys_enter_write) {
 
     if (args->count == 0)
         return 0;
+
+    struct event *event;
     const char *user_buf = args->buf;
 
-    struct event *event = buffer.ringbuf_reserve(sizeof(struct event));
-    if (!event) {
-        bpf_trace_printk("FAILED RESERVING SLOT IN BUFFER\\n");
-        return 1;
-    }
+    @SUBMIT_EVENTS
 
-    /* 1st PART */
-    max_read_len = (args->count > BUF_SIZE) ? BUF_SIZE : args->count;
-    len = bpf_probe_read_user_str(event->buf, max_read_len, user_buf);
-    if (len < 0) {
-        bpf_trace_printk("FAILED READING USER STRING\\n");
-        buffer.ringbuf_discard(event, 0);
-        return 1;
-    } else {
-        bpf_trace_printk("SUBMIT EVENT\\n");
-        event->count = args->count;
-        event->fd = args->fd;
-        event->len = len;
-        event->off = 0;
-        buffer.ringbuf_submit(event, 0);
-    }
-    off += len;
-    if (off >= args->count)
-        return 0;
-
-    @SUBMIT_NEXT_EVENTS
-
-    /* THE REST */
+    /* handle the left text if there is any
+       (if we haven't terminated the program in SUBMIT_EVENTS) */
     event = buffer.ringbuf_reserve(sizeof(struct event));
     if (!event) {
         bpf_trace_printk("FAILED RESERVING SLOT (2) IN BUFFER\\n");
@@ -102,14 +80,15 @@ TRACEPOINT_PROBE(syscalls, sys_enter_write) {
     event->len = -1;
     event->off = off;
     buffer.ringbuf_submit(event, 0);
+    bpf_trace_printk("TOO LONG\n", 10);
 
     return 0;
 }
-""".replace("@SUBMIT_NEXT_EVENTS", 15*submit_next_event)
+""".replace("@SUBMIT_EVENTS", 18*submit_next_event)
 
 b = BPF(text=src)
 get_data = b['buffer'].event
-partial = ""
+partial = b""
 
 def printstderr(x): print(x, file=stderr)
 
@@ -129,57 +108,78 @@ evid = 1
 
 waiting_for_buffer = 0
 
-print(f"Compiling regex `{regex}`...", end="")
-#regexc = regex_compile(regex)
-print(" done")
+regexc = regex_compile(regex)
 
-def parse_line(line):
-    return
-    printstderr("LINE: " + line)
-    m = regexc.match(line)
-    print(m)
-
-def callback(ctx, data, size):
-    global partial
+def parse_lines(buff):
     global evid
-    event = get_data(data)
-    s = event.buf.decode('utf-8', 'ignore')
-    if event.len == event.count:
-        printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m:")
+    for line in buff.splitlines():
+        printstderr(f"> \033[33;2m`{line}`\033[0m")
+        m = regexc.match(line)
+        if not m:
+            continue
+
+        n = 1
+
+        # start a write into the buffer
         addr = buffer_start_push(shmbuf)
         while not addr:
             addr = buffer_start_push(shmbuf)
             global waiting_for_buffer
             waiting_for_buffer += 1
-
         # push the base data (kind and event ID)
         addr = buffer_partial_push(shmbuf, addr, kind.to_bytes(8, "little"), 8)
         addr = buffer_partial_push(shmbuf, addr, evid.to_bytes(8, "little"), 8)
 
-        if partial:
-            printstderr(f"{partial}{s}")
-            addr = buffer_partial_push_str(shmbuf, addr, evid, f"{partial}{s}")
-            partial = ""
-        else:
-            printstderr(s)
-            addr = buffer_partial_push_str(shmbuf, addr, evid, s)
+        for o in signature:
+            printstderr("match: " + str(m[n]))
+            if o == 'i':
+                addr = buffer_partial_push(shmbuf, addr,
+                                           int(m[n]).to_bytes(4, "little"), 4)
+            elif o == 'l':
+                addr = buffer_partial_push(shmbuf, addr,
+                                           int(m[n]).to_bytes(8, "little"), 8)
+            elif o == 'S':
+                addr = buffer_partial_push_str(shmbuf, addr, evid, m[n])
+            elif o == 'M':
+                addr = buffer_partial_push_str(shmbuf, addr, evid, m[0])
+            else:
+                raise NotImplementedError("Not implemented signature op")
+            n += 1
+
         buffer_finish_push(shmbuf)
+        printstderr(f"sent ev `{evid}`")
         evid += 1
+
+def callback(ctx, data, size):
+    global partial
+
+    event = get_data(data)
+    s = event.buf
+    printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, "\
+                f"off: {event.off}, len: {event.len}]\033[0m")
+    if event.len == event.count:
+        buff = None
+        if partial:
+            buff = partial + s
+            partial = b""
+        else:
+            buff = s
+
+        assert buff is not None
+        parse_lines(buff.decode("utf-8", "ignore"))
     elif event.len < event.count:
-        printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m")
         if event.len == -1:
-            printstderr(f"{partial}{s}")
+            printstderr(partial + s)
             printstderr(f"... TEXT TOO LONG, DROPPED {event.count} CHARS")
-            partial = ""
+            partial = b""
             raise NotImplementedError("Unhandled situation")
         else:
             partial += s
     else:
-        printstderr(f"\033[34;1m[fd: {event.fd}, count: {event.count}, off: {event.off}, len: {event.len}]\033[0m")
         raise NotImplementedError("Unhandled situation")
 
-b['buffer'].open_ring_buffer(callback)
 
+b['buffer'].open_ring_buffer(callback)
 
 try:
     while 1:
