8037d467b55899deb79e982e674a52bee2be37a7
Marek Chalupa <mchqwerty@gmail.com>
2022-08-15 09:48:10 +0200

    runprimes.sh: check for `primes` executable

--- experiments/primes/runprimes.sh
+++ experiments/primes/runprimes.sh
@@ -7,6 +7,14 @@ SOURCESDIR="$DIR/../../sources"
 REGEXSOURCE=$SOURCESDIR/regex
 NUM=$1
 test -z $NUM && NUM=10000
+
+# check that `primes` exists and is executable
+if [ ! -x $DIR/primes ]; then
+	echo "'primes' executable does not exist or has no x permission"
+	echo "maybe run 'make' in '${DIR}'?"
+	exit 1
+fi
+
 $DIR/primes $NUM | $REGEXSOURCE /primes1 prime "#([0-9]+):\s*([0-9]+)\s*$" ii&
 $DIR/primes $NUM | $REGEXSOURCE /primes2 prime "#([0-9]+):\s*([0-9]+)\s*$" ii&
 
