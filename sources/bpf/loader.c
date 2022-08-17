e6beb006eb396361c5a573fd13f8287299bcb348
Marek Chalupa <mchqwerty@gmail.com>
2022-08-17 12:40:25 +0200

    sources/bpf: progress

--- sources/bpf/loader.c
+++ sources/bpf/loader.c
@@ -3,50 +3,68 @@
 #include <unistd.h>
 
 struct event {
-  __u32 pid;
-  char filename[16];
+    __u32 pid;
+    char filename[16];
 };
 
 static int buf_process_sample(void *ctx, void *data, size_t len) {
-  struct event *evt = (struct event *)data;
-  printf("%d %s\n", evt->pid, evt->filename);
+    struct event *evt = (struct event *)data;
+    printf("%d %s\n", evt->pid, evt->filename);
 
-  return 0;
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-  const char *file = "program.bpf";
-  struct bpf_object *obj;
-  int prog_fd = -1;
-  int buffer_map_fd = -1;
-  struct  bpf_program *prog;
+    int prog_fd, map;
+    const char *bpf_file = "program.bpf";
+    struct bpf_object *obj;
+    struct bpf_program *prog;
+    struct ring_buffer *ring_buffer;
 
-  bpf_prog_load(file, BPF_PROG_TYPE_TRACEPOINT, &obj, &prog_fd);
+    obj = bpf_object__open(bpf_file);
+    if (libbpf_get_error(obj)) {
+        perror("failed loading BPF program");
+        return 1;
+    }
 
-  buffer_map_fd = bpf_object__find_map_fd_by_name(obj, "buffer");
+    prog = bpf_object__find_program_by_name(
+        obj, "tracepoint__syscalls__sys_enter_write");
+    if (!prog) {
+        fprintf(stderr, "Failed finding the program\n");
+        return 1;
+    }
 
-  struct ring_buffer *ring_buffer;
+    bpf_program__set_type(prog, BPF_PROG_TYPE_TRACEPOINT);
+    int err = bpf_object__load(obj);
+    if (err < 0) {
+        perror("failed loading BPF object");
+        return 1;
+    }
+    prog_fd = bpf_program__fd(prog);
 
-  ring_buffer = ring_buffer__new(buffer_map_fd, buf_process_sample, NULL, NULL);
+    map = bpf_object__find_map_fd_by_name(obj, "buffer");
+    if (map < 0) {
+        fprintf(stderr, "buffer not found\n");
+        return 1;
+    }
 
-  if(!ring_buffer) {
-    fprintf(stderr, "failed to create ring buffer\n");
-    return 1;
-  }
+    ring_buffer = ring_buffer__new(map, buf_process_sample, NULL, NULL);
 
-  prog = bpf_object__find_program_by_title(obj, "tracepoint/syscalls/sys_enter_execve");
-  if (!prog) {
-    fprintf(stderr, "failed to find tracepoint\n");
-    return 1;
-  }
+    if (!ring_buffer) {
+        fprintf(stderr, "failed to create ring buffer\n");
+        return 1;
+    }
 
-  bpf_program__attach_tracepoint(prog, "syscalls", "sys_enter_execve");
+    bpf_program__attach(prog);
 
-  while(1) {
-    ring_buffer__consume(ring_buffer);
-    sleep(1);
-  }
+    /*
+    bpf_program__attach_tracepoint(prog, "syscalls", "sys_enter_execve");
+    */
 
-  return 0;
+    while (1) {
+        ring_buffer__consume(ring_buffer);
+        sleep(1);
+    }
+
+    return 0;
 }
-
--- sources/bpf/program.bpf.c
+++ sources/bpf/program.bpf.c
@@ -1,47 +1,104 @@
-#include <linux/types.h>
+#include "vmlinux.h"
 
 #include <bpf/bpf_helpers.h>
-#include <linux/bpf.h>
+#include <bpf/bpf_tracing.h>
+
+#define BUF_SIZE 255
 
 struct event {
-  __u32 pid;
-  char filename[16];
+    char buf[BUF_SIZE];
+    int count;
+    int len;
+    int off;
+    int fd;
 };
 
-struct bpf_map_def SEC("maps") buffer = {
-    .type = BPF_MAP_TYPE_RINGBUF,
-    .max_entries = 4096 * 64,
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 4096 * 64);
+} buffer SEC(".maps");
+
+/*
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 4);
+} info SEC(".maps");
+
+static int handle_dropping(void) {
+    int zero = 0;
+    u64 *dropped = bpf_map_lookup_elem(&info, &zero);
+    if (dropped) {
+        u64 newval = *dropped + 1;
+        bpf_map_update_elem(&info, &zero, &newval, 0);
+    } else {
+        u64 newval = 1;
+        bpf_map_update_elem(&info, &zero, &newval, 0);
+        return 1;
+    }
+    return 0;
+}
+*/
+
+struct syscall_enter_write_args{
+    unsigned long long unused;
+    long __syscall_nr;
+    unsigned long long fd;
+    unsigned long long buf;
+    unsigned long long count;
 };
 
-struct trace_entry {
-  short unsigned int type;
-  unsigned char flags;
-  unsigned char preempt_count;
-  int pid;
-};
+SEC("tracepoint/syscalls/sys_enter_write")
+int tracepoint__syscalls__sys_enter_write(struct trace_event_raw_sys_enter *ctx) {
+    int ret;
+    int zero = 0;
+    unsigned int len, off = 0;
 
-struct trace_event_raw_sys_enter {
-  struct trace_entry ent;
-  long int id;
-  long unsigned int args[6];
-  char __data[0];
-};
+    bpf_printk("BPF triggered\n");
+    /*
+    int pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_printk("BPF triggered from the PID %d.\n", pid);
 
+    if ((bpf_get_current_pid_tgid() >> 32) != @PID) {
+        return 0;
+    }
 
-SEC("tracepoint/syscalls/sys_enter_execve")
-int sys_enter_execve(struct trace_event_raw_sys_enter *ctx) {
-  __u32 pid = bpf_get_current_pid_tgid() >> 32;
-  struct event *event = bpf_ringbuf_reserve(&buffer, sizeof(struct event), 0);
-  if (!event) {
-    return 1;
-  }
-  event->pid = pid;
-  bpf_probe_read_user_str(event->filename, sizeof(event->filename),
-                          (const char *)ctx->args[0]);
+    struct syscall_enter_write_args* args = (struct syscall_enter_write_args*)ctx;
+    int fd = args->fd;
+    if (fd != 1) {
+        return 0; // not interested
+    }
 
-  bpf_ringbuf_submit(event, 0);
+    int count = args->count;
+    if (count == 0)
+        return 0;
 
-  return 0;
+    struct event *event;
+    const char *user_buf = (void*)args->buf;
+    */
+
+#if 0
+    /* @SUBMIT_EVENTS */
+
+    /* handle the left text if there is any
+       (if we haven't terminated the program in SUBMIT_EVENTS) */
+    event = buffer.ringbuf_reserve(sizeof(struct event));
+    if (!event) {
+        bpf_trace_printk("FAILED RESERVING SLOT (2) IN BUFFER\\n");
+        return 1;
+    }
+
+    /* Tell monitor that the text was too long */
+    event->buf[0] = 0;
+    event->count = args->count - off;
+    event->fd = args->fd;
+    event->len = -1;
+    event->off = off;
+    buffer.ringbuf_submit(event, 0);
+    bpf_trace_printk("TOO LONG\n", 10);
+#endif
+
+    return 0;
+
 }
 
 char _license[] SEC("license") = "GPL";
