0aac25e6f196badd1dfabeab9f5a21d797ff7cff
smml1996 <michelle_muroya@hotmail.com>
2022-09-16 22:53:50 +0200

    free pointers chosen_streams

--- compiler/main.py
+++ compiler/main.py
@@ -184,6 +184,11 @@ int main(int argc, char **argv) {"{"}
 	// destroy arbiter buffers
 {destroy_buffers()}
 	
+	// free chosen_streams
+	for(int i = 0; i < {get_count_events_sources()}; i++) {"{"}
+		free(chosen_streams[i];
+	{"}"}
+	free(chosen_streams);
 	{get_pure_c_code(components, 'cleanup')}
 {"}"}
 '''
