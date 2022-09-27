aecf34b6c626c495e92f95443a3d2509b498d4d8
Marek Chalupa <mchqwerty@gmail.com>
2022-09-27 13:10:46 +0200

    experiments/scalability: fix the makefile

--- experiments/scalability/experiments.sh
+++ experiments/scalability/experiments.sh
@@ -9,10 +9,12 @@ rm -f log.txt
 SRCDIR="$(dirname $0)/../.."
 SHM_BUFSIZE_FILE="${SRCDIR}/shmbuf/buffer-size.h"
 
+
 for SHM_BUFSIZE in 1 8 16 32; do
 	make clean
         make clean -j -C $SRCDIR
         sed -i "s/#define\\s*SHM_BUFFER_SIZE_PAGES.*/#define SHM_BUFFER_SIZE_PAGES $SHM_BUFSIZE/" $SHM_BUFSIZE_FILE
         make -j -C $SRCDIR
+	make source -B
         ./run.sh $SHM_BUFSIZE
 done
