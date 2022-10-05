fd33f88fe39461cc5e79a10f935e6ebd67614462
Marek Chalupa <mchqwerty@gmail.com>
2022-10-05 16:02:26 +0200

    runprimes-dr.sh: put drrun into variable

--- experiments/primes/runprimes-dr.sh
+++ experiments/primes/runprimes-dr.sh
@@ -4,15 +4,15 @@ SOURCESDIR="$(pwd)/../../sources"
 NUM=$1
 test -z $NUM && NUM=10000
 
-/opt/dynamorio//build/bin64/drrun\
-	-root /opt/dynamorio//build/\
+DRRUN=/opt/dynamorio//build/bin64/drrun
+
+$DRRUN -root /opt/dynamorio//build/\
 	-opt_cleancall 2 -opt_speed\
 	-c /opt/shamon/sources/drregex/libdrregex.so\
 	/primes1 prime '#([0-9]+): ([0-9]+)' ii --\
 	./primes $NUM &
 
-/opt/dynamorio//build/bin64/drrun\
-	-root /opt/dynamorio//build/\
+$DRRUN -root /opt/dynamorio//build/\
 	-opt_cleancall 2 -opt_speed\
 	-c /opt/shamon/sources/drregex/libdrregex.so\
 	/primes2 prime '#([0-9]+): ([0-9]+)' ii --\
