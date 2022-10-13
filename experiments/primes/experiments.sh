b8476b99cde9a6b41e9909887ede30acb54e7358
Stefanie MUROYA LEI <smuroyal@lntohe020.ist.local>
2022-10-13 11:25:44 +0200

    setup primes experiments for autodrop

--- experiments/primes/experiments.sh
+++ experiments/primes/experiments.sh
@@ -17,7 +17,7 @@ for SHM_BUFSIZE in 32; do
         make -j  -C $SRCDIR
         #for ARBITER_BUFSIZE in 8 16 64 128; do
         for DROP_WAIT in 0.05 0.1 0.2 0.25 0.4 0.5; do
-            for ARBITER_BUFSIZE in 4 16 128 1024; do
+            for ARBITER_BUFSIZE in 1024; do
                     for PRIMES_NUM in 10000 20000 30000 40000; do
                             ./run.sh $SHM_BUFSIZE $ARBITER_BUFSIZE $PRIMES_NUM $DROP_WAIT
                     done
