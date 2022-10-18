1eb3986456596576aba813551815b036b2f4eb57
Marek Chalupa <mchqwerty@gmail.com>
2022-10-18 14:13:44 +0200

    bank: fix sync in experiments

--- experiments/bank/regexbank.sh
+++ experiments/bank/regexbank.sh
@@ -20,6 +20,11 @@ mkfifo /tmp/fifo{A,B}
 
 python inputs.py $NUM > inputs.last.txt
 
+# we can start it a bit before as it is waiting for the connection
+echo "-- Starting the monitor --"
+$MONITOR Out:regex:/bank.stdout In:regex:/bank.stdin&
+MON_PID=$!
+
 $DRRUN -t /bank \
 balance "\s*Current balance on Account ([0-9]+):\s*" i \
 depositTo "\s*Deposit to Account ([0-9]+)" i \
@@ -35,19 +40,7 @@ numOut "^\s*([0-9]+)\s*$" i \
 logout "Logged out!" $'' \
 -stdin \
 numIn "^\s*([0-9]+)\s*$" i \
-otherIn ".*[^\s].*" $'' -- $(dirname $0)/bank $@ < /tmp/fifoA  > /tmp/fifoB 2>source-log.txt &
-
-echo "-- Starting the monitor --"
-$MONITOR Out:regex:/bank.stdout In:regex:/bank.stdin&
-MON_PID=$!
-
-# wait for the source and monitor to be ready
-tail -n 1000 -f source-log.txt | while read line; do
-    if echo $line | grep -L "Continue program"; then
-            echo "All ready!"
-            break
-    fi
-done
+otherIn ".*[^\s].*" $'' -- $(dirname $0)/bank $@ < /tmp/fifoA  > /tmp/fifoB &
 
 echo "-- Starting interact --"
 cat /tmp/fifoB | ./interact inputs.last.txt interact.log >/tmp/fifoA &
