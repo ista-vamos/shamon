9c91b729fa623a2542b7a5382e1d7843f5b6eb09
Marek Chalupa <marek.chalupa@ist.ac.at>
2022-10-21 00:59:11 +0200

    primes comparison tessla vs vamos times

--- experiments/tessla_compare/times_primes.sh
+++ experiments/tessla_compare/times_primes.sh
@@ -15,6 +15,12 @@ for i in `seq 1 $REP`; do
     wait
     wait
 
+    /bin/time -f '%e' -o tmp.file experiments/tessla_compare/primes_tessla/target/debug/tessla_monitor < experiments/tessla_compare/gen_tessla_input/primes.in
+    printf "no_errors,%s,tessla\n" "$(<tmp.file)" >> primes_times.csv
+
+    /bin/time -f '%e' -o tmp.file experiments/tessla_compare/primes_tessla/target/debug/tessla_monitor < experiments/tessla_compare/gen_tessla_input/primes_bad.in
+    printf "errors,%s,tessla\n" "$(<tmp.file)" >> primes_times.csv
+
 done
 
 mv primes_times.csv experiments/tessla_compare/primes_times.csv
