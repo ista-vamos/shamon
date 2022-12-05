834ed8edb7646e0c40e86543029d64a71d4bf575
Marek Chalupa <mchqwerty@gmail.com>
2022-12-05 11:46:24 +0100

    race-instr: include llvm/IR/CFG.h
    For succ_begin/end.

--- sources/llvm/RaceInstrumentation.cpp
+++ sources/llvm/RaceInstrumentation.cpp
@@ -4,6 +4,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/CFG.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "llvm/IR/LegacyPassManager.h"
