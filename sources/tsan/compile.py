0e3001618d960bd6c0d6614a6979d903533ecc24
Marek Chalupa <mchqwerty@gmail.com>
2022-12-30 16:02:58 +0100

    Add a datarace test

--- sources/tsan/compile.py
+++ sources/tsan/compile.py
@@ -30,6 +30,8 @@ def get_args(argv):
     cc="clang"
     output="a.out"
     flags=[]
+    tolink=[]
+    openmp = None
 
     i = 1
     while i < len(argv):
@@ -39,20 +41,26 @@ def get_args(argv):
         elif argv[i] == "-cc":
             i += 1
             cc = argv[i]
+        elif argv[i] == "-l":
+            i += 1
+            tolink.append(argv[i])
+        elif argv[i] == "-omp":
+            i += 1
+            openmp = argv[i]
         else:
             if prog is None:
                 prog = argv[i]
             else:
                 flags.append(argv[i])
         i += 1
-    return prog, cc, abspath(output), flags
+    return prog, cc, abspath(output), flags, tolink, openmp
 
 def cmd(args):
     print("> ", " ".join(args))
     run(args, check=True)
 
 def main(argv):
-    prog, cc, output, flags = get_args(argv)
+    prog, cc, output, flags, tolink, omp = get_args(argv)
     flags = CFLAGS + flags
 
    #print(f"Compiler: {cc}")
@@ -60,12 +68,14 @@ def main(argv):
    #print(f"Flags: {flags}")
    #print(f"Output: {output}")
 
-    cmd([cc, "-emit-llvm", "-c", "-fsanitize=thread", "-o", f"{output}.tmp1.bc", prog] + flags)
+    cmd([cc, "-emit-llvm", "-c", "-fsanitize=thread", "-O0", "-o", f"{output}.tmp1.bc", prog] + flags)
+
     cmd([opt, "-enable-new-pm=0", "-load",  f"{LLVM_PASS_DIR}/race-instrumentation.so",
               "-vamos-race-instrumentation", f"{output}.tmp1.bc", "-o", f"{output}.tmp2.bc"])
     cmd([cc, "-std=c11", "-emit-llvm", "-c", f"{DIR}/tsan_impl.c", "-o", f"{DIR}/tsan_impl.bc"] + flags + SHAMON_INCLUDES)
-    cmd([link, f"{DIR}/tsan_impl.bc", f"{output}.tmp2.bc", "-o", f"{output}.tmp3.bc"])
-    cmd([cc, "-pthread", f"{output}.tmp3.bc", "-o", output] + flags + SHAMON_LIBS)
+    cmd([link, f"{DIR}/tsan_impl.bc", f"{output}.tmp2.bc", "-o", f"{output}.tmp3.bc"] + tolink)
+    cmd([opt, "-O3", f"{output}.tmp3.bc", "-o", f"{output}.tmp4.bc"])
+    cmd([cc, "-pthread", f"{output}.tmp4.bc", "-o", output] + flags + SHAMON_LIBS)
 
 
 
