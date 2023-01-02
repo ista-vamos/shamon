d4f93fa54f5dbce2f19122f982a1a41ccba147b8
Marek Chalupa <mchqwerty@gmail.com>
2023-01-02 13:30:47 +0100

    tsan/compile.py: extend options
    Allow define what to link and instrument.

--- sources/tsan/compile.py
+++ sources/tsan/compile.py
@@ -4,79 +4,135 @@ import sys
 from subprocess import run
 from os.path import dirname, abspath
 
-DIR=dirname(sys.argv[0])
-SHAMONDIR=f"{DIR}/../.."
-LLVM_PASS_DIR=f"{DIR}/../llvm"
-CFLAGS=[
-    "-DDEBUG_STDOUT", "-g", #"-O3"
-    ]
-SHAMON_INCLUDES=[f"-I{DIR}/../../"]
-SHAMON_LIBS=[
+DIR = dirname(sys.argv[0])
+SHAMONDIR = f"{DIR}/../.."
+LLVM_PASS_DIR = f"{DIR}/../llvm"
+CFLAGS = [
+    "-DDEBUG_STDOUT",
+    "-g",  # "-O3"
+]
+SHAMON_INCLUDES = [f"-I{DIR}/../../"]
+SHAMON_LIBS = [
     f"{SHAMONDIR}/core/libshamon-arbiter.a",
     f"{SHAMONDIR}/shmbuf/libshamon-shmbuf.a",
     f"{SHAMONDIR}/core/libshamon-utils.a",
     f"{SHAMONDIR}/core/libshamon-stream.a",
     f"{SHAMONDIR}/core/libshamon-parallel-queue.a",
     f"{SHAMONDIR}/core/signatures.c",
-    f"{SHAMONDIR}/streams/libshamon-streams.a"
+    f"{SHAMONDIR}/streams/libshamon-streams.a",
 ]
 
 opt = "opt"
 link = "llvm-link"
 
 
-def get_args(argv):
-    prog=None
-    cc="clang"
-    output="a.out"
-    flags=[]
-    tolink=[]
-    openmp = None
+class CompileOptions:
+    def __init__(self):
+        self.files = []
+        self.cc = "clang"
+        self.output = "a.out"
+        self.cflags = []
+        self.link = []
+        self.link_asm = []
+        self.link_and_instrument = []
+
+
+def get_opts(argv):
+    opts = CompileOptions()
 
     i = 1
     while i < len(argv):
         if argv[i] == "-o":
             i += 1
-            output = argv[i]
+            opts.output = argv[i]
         elif argv[i] == "-cc":
             i += 1
-            cc = argv[i]
+            opts.cc = argv[i]
+        elif argv[i] == "-li":
+            i += 1
+            opts.link_and_instrument.append(argv[i])
         elif argv[i] == "-l":
             i += 1
-            tolink.append(argv[i])
+            opts.link.append(argv[i])
+        elif argv[i] == "-I":
+            i += 1
+            opts.cflags.append(f"-I{argv[i]}")
         elif argv[i] == "-omp":
             i += 1
-            openmp = argv[i]
+            opts.link_and_instrument.append(argv[i])
+            opts.cflags.append("-fopenmp")
         else:
-            if prog is None:
-                prog = argv[i]
+            s = argv[i]
+            if any((s.endswith(suffix) for suffix in (".c", ".cpp", ".i"))):
+                opts.files.append(s)
+            elif any((s.endswith(suffix) for suffix in (".bc", ".ll"))):
+                opts.link_and_instrument.append(s)
+            elif s.endswith(".S"):
+                opts.link_asm.append(s)
             else:
-                flags.append(argv[i])
+                opts.cflags.append(s)
         i += 1
-    return prog, cc, abspath(output), flags, tolink, openmp
+    return opts
+
 
 def cmd(args):
     print("> ", " ".join(args))
     run(args, check=True)
 
+
 def main(argv):
-    prog, cc, output, flags, tolink, omp = get_args(argv)
-    flags = CFLAGS + flags
+    opts = get_opts(argv)
+    assert opts.files, "No input files given"
 
-   #print(f"Compiler: {cc}")
-   #print(f"Program: {prog}")
-   #print(f"Flags: {flags}")
-   #print(f"Output: {output}")
+    output = opts.output
+    compiled_files = [f"{file}.bc" for file in opts.files]
+    for f, out in zip(opts.files, compiled_files):
+        cmd(
+            [opts.cc, "-emit-llvm", "-c", "-fsanitize=thread", "-O0", "-o", f"{out}", f]
+            + CFLAGS
+            + opts.cflags
+        )
+    cmd([link, "-o", f"{output}.tmp2.bc"] + compiled_files + opts.link_and_instrument)
 
-    cmd([cc, "-emit-llvm", "-c", "-fsanitize=thread", "-O0", "-o", f"{output}.tmp1.bc", prog] + flags)
+    cmd(
+        [
+            opt,
+            "-enable-new-pm=0",
+            "-load",
+            f"{LLVM_PASS_DIR}/race-instrumentation.so",
+            "-vamos-race-instrumentation",
+            f"{output}.tmp2.bc",
+            "-o",
+            f"{output}.tmp3.bc",
+        ]
+    )
 
-    cmd([opt, "-enable-new-pm=0", "-load",  f"{LLVM_PASS_DIR}/race-instrumentation.so",
-              "-vamos-race-instrumentation", f"{output}.tmp1.bc", "-o", f"{output}.tmp2.bc"])
-    cmd([cc, "-std=c11", "-emit-llvm", "-c", f"{DIR}/tsan_impl.c", "-o", f"{DIR}/tsan_impl.bc"] + flags + SHAMON_INCLUDES)
-    cmd([link, f"{DIR}/tsan_impl.bc", f"{output}.tmp2.bc", "-o", f"{output}.tmp3.bc"] + tolink)
-    cmd([opt, "-O3", f"{output}.tmp3.bc", "-o", f"{output}.tmp4.bc"])
-    cmd([cc, "-pthread", f"{output}.tmp4.bc", "-o", output] + flags + SHAMON_LIBS)
+    cmd(
+        [
+            opts.cc,
+            "-std=c11",
+            "-emit-llvm",
+            "-c",
+            f"{DIR}/tsan_impl.c",
+            "-o",
+            f"{DIR}/tsan_impl.bc",
+        ]
+        + CFLAGS
+        + SHAMON_INCLUDES
+    )
+    cmd(
+        [link, f"{DIR}/tsan_impl.bc", f"{output}.tmp3.bc", "-o", f"{output}.tmp4.bc"]
+        + opts.link
+    )
 
+    cmd([opt, "-O3", f"{output}.tmp4.bc", "-o", f"{output}.tmp5.bc"])
+    cmd(
+        [opts.cc, "-pthread", f"{output}.tmp5.bc", "-o", output]
+        + opts.link_asm
+        + opts.cflags
+        + CFLAGS
+        + SHAMON_LIBS
+    )
 
 
 if __name__ == "__main__":
