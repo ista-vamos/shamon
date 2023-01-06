5eaf995832654c6a07ff439aa41e0f05c3cf52d6
Marek Chalupa <mchqwerty@gmail.com>
2023-01-06 01:50:44 +0100

    experiments/bank: use global config

--- experiments/bank/run.sh
+++ experiments/bank/run.sh
@@ -2,31 +2,28 @@
 
 set -e
 
-NUM=100000
+test -z BANK_NUM && BANK_NUM=100000
 
-REP=1
 export PYTHON_OPTIMIZE=1
 #export PYTHONDONTWRITEBYTECODE=1
 
-for i in `seq 1 $REP`; do
-    for ARBITER_BUFSIZE in 4 16 32 64 128 256; do
-	echo "** SHM BUFSIZE: 1000 elems, ARBITER_BUFSIZE: $ARBITER_BUFSIZE **"
-	echo " - Generating the monitor"
-	./genmon.py bankmonitor.vl.in $ARBITER_BUFSIZE
-	make -j4 bank 1>>/tmp/make.stdout.txt 2>>/tmp/make.stderr.txt
-	make -j4 interact 1>>/tmp/make.stdout.txt 2>>/tmp/make.stderr.txt
-	make -j4 monitor-vamos 1>>/tmp/make.stdout.txt 2>>/tmp/make.stderr.txt
+for ARBITER_BUFSIZE in 4 16 32 64 128 256; do
+    echo "** SHM BUFSIZE: 1000 elems, ARBITER_BUFSIZE: $ARBITER_BUFSIZE **"
+    echo " - Generating the monitor"
+    ./genmon.py bankmonitor.vl.in $ARBITER_BUFSIZE
+    make -j4 bank 1>>/tmp/make.stdout.txt 2>>/tmp/make.stderr.txt
+    make -j4 interact 1>>/tmp/make.stdout.txt 2>>/tmp/make.stderr.txt
+    make -j4 monitor-vamos 1>>/tmp/make.stdout.txt 2>>/tmp/make.stderr.txt
 
-	echo " - Running"
-	rm -f out.txt err.txt
-	./regexbank.sh $NUM >out.txt 2>err.txt
-	./parse_results.py out.txt 0 >> bank-1-$ARBITER_BUFSIZE-$NUM.csv
+    echo " - Running"
+    rm -f out.txt err.txt
+    ./regexbank.sh $BANK_NUM >out.txt 2>err.txt
+    ./parse_results.py out.txt 0 >> bank-1-$ARBITER_BUFSIZE-$BANK_NUM.csv
 
-	for ERR_FREQ in 5 10 100 1000; do
-	    echo " - Running with $ERR_FREQ errors freq"
-	    ./regexbank.sh $NUM errors $ERR_FREQ >out.txt 2>err.txt
-	    ./parse_results.py out.txt $ERR_FREQ >> bank-1-$ARBITER_BUFSIZE-$NUM.csv
-	done
+    for ERR_FREQ in 5 10 100 1000; do
+        echo " - Running with $ERR_FREQ errors freq"
+        ./regexbank.sh $BANK_NUM errors $ERR_FREQ >out.txt 2>err.txt
+        ./parse_results.py out.txt $ERR_FREQ >> bank-1-$ARBITER_BUFSIZE-$BANK_NUM.csv
     done
 done
 
--- experiments/setup.sh
+++ experiments/setup.sh
@@ -2,6 +2,7 @@
 REPEAT=10   #
 SCALABILITY_NUM=10000000
 PRIMES_10000="no"
+BANK_NUM=100000
 DATARACES_HARNESS_NO=3
 
 
@@ -9,6 +10,7 @@ DATARACES_HARNESS_NO=3
 REPEAT=1                # how many time repeat each experiment
 SCALABILITY_NUM=10000   # how many events emit in scalability experiments
 PRIMES_10000="yes"      # run primes experiments with 10000 only
+BANK_NUM=10000          # the (approximate) number of generated commands
 DATARACES_HARNESS_NO=1  # how many harnesses use for each datarace test
 
 
