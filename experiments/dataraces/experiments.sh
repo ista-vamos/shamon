40a1bfffc5d524d4e9f32853cd03b0aa6a3fd84d
Marek Chalupa <mchqwerty@gmail.com>
2023-01-06 02:03:32 +0100

    experiments/dataraces: use global config

--- experiments/dataraces/experiments.sh
+++ experiments/dataraces/experiments.sh
@@ -7,8 +7,16 @@ REP=3
 cd $(dirname $0)
 rm -f results-*.csv
 
-for I in `seq 1 $REP`; do
-	for FILE in $(ls benchmarks/*.i); do
-		python3 ./run.py $FILE results-$I.csv;
-	done
+source ../setup.sh
+
+for I in `seq 1 $DATARACES_HARNESS_NUM`; do
+	if [ "$DATARACES_SHORT" = "yes" ]; then
+		for FILE in $(ls benchmarks/fib*.i); do
+			python3 ./run.py $FILE results.csv;
+		done
+	else
+		for FILE in $(ls benchmarks/*.i); do
+			python3 ./run.py $FILE results.csv;
+		done
+	fi
 done
